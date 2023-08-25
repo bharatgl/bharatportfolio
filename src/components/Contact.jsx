7c7e76f46302140d38f1a7ad9bfabca7eaf85654
bharatgl <goyalbharat9977@gmail.com>
2023-08-26 01:19:34 +0530

    fixed keys

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -41,10 +41,10 @@ const Contact = () => {
 //service_bj4xoi7
     emailjs
       .send(
-        "service_bj4xoi7",
-        "template_kfinsin",
-        // import.meta.env.VITE_APP_EMAILJS_SERVICE_ID,
-        // import.meta.env.VITE_APP_EMAILJS_TEMPLATE_ID,
+        // "service_bj4xoi7",
+        // "template_kfinsin",
+        import.meta.env.VITE_APP_EMAILJS_SERVICE_ID,
+        import.meta.env.VITE_APP_EMAILJS_TEMPLATE_ID,
         {
           from_name: form.name,
           to_name: "Bharat Goyal",
